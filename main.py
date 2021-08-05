9b9b342d0450c30af62ebf627952885903ae7a98
Jonah Grosshanten <s8jhgros@stud.uni-saarland.de>
2021-08-05 14:19:24 +0200

    14:19

--- main.py
+++ main.py
@@ -7,37 +7,36 @@ import json
 path ="/"
 dirs = os.listdir(path)
 dirs.sort()
-checks = []
+file_list = []
 # Liste an zu kontrollierenden Files erstellen
 for file in dirs:
     if file.endswith('.pdf'):
-        checks.append(file)
+        file_list.append(file)
 
-# define keyterms
-String1 = "Rechnung"
-String2 = "Angebot"
-String4 = "Test"
-for check in checks:
-    pdfFileObj = open(check, 'rb')
-    # open the pdf file
-    object = PyPDF4.PdfFileReader(pdfFileObj)
-    # get number of pages
-    NumPages = object.getNumPages()
-    # extract text and do the search
-    for i in range(0, NumPages):
-        PageObj = object.getPage(i)
-        Text = PageObj.extractText()
+def save_file_temp (source,file,category, date):
+    destination =
+    shutil.move(f"{source}/{file}", destination)
+
+def get_cat_and_date(file_list):
+    # define keyterms
+    Rechnung = "Rechnung"
+    Angebot = "Angebot"
+    for file in file_list:
+        pdfFileObj = open(file, 'rb')
+        # open the pdf file
+        object = PyPDF4.PdfFileReader(pdfFileObj)
+        # extract text and do the search
+        PageObj = object.getPage(0)
+        text = PageObj.extractText()
         # print(Text)
-        ResSearch1 = search(String1, Text)
-        ResSearch2 = search(String1, Text)
-        if ResSearch1=="Rechnung":
-            # TODO Dann kontrolliere nach Datum
-            #anschließend speichere es ab
-            pass
-        if ResSearch2== "Angebot":
-            # TODO s.o
-            pass
-
+        category_R = search(Rechnung, text)
+        catergory_A = search(Angebot, text)
+        if category_R == "Rechnung":
+            date = search(r'\d{2}-\d{2}-\d{4}', text)
+            save_file(file,category_R,date)
+        if catergory_A == "Angebot":
+            date = search(r'\d{2}-\d{2}-\d{4}', text)
+            save_file(file, catergory_A, date)
 
 def get_temp_files():
 
@@ -57,14 +56,23 @@ def get_temp_files():
 
     return list
 
-
+def set_companies(company_name):
+    f = open('companies.json', )
+    data = json.load(f)
+    data.append(company_name)
+    with open('companies.json', "w") as company:
+        json.dump(data, company)
 def get_companies():
     f = open('companies.json', )
     data = json.load(f)
     f.close()
     return data
-
-
+def set_projects(project_name):
+    f = open('projects.json', )
+    data = json.load(f)
+    data.append(project_name)
+    with open('projects.json', "w") as project:
+        json.dump(data, project)
 def get_projects():
     f = open('projects.json', )
     data = json.load(f)
