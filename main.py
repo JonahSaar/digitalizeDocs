59c848f16dd32b55dae56c1b1a69a800d73e1b46
Jonah Grosshanten <s8jhgros@stud.uni-saarland.de>
2021-08-05 12:15:13 +0200

    12:15

---
+++ main.py
@@ -0,0 +1,40 @@
+import os
+from re import search
+import datefinder
+import PyPDF4
+
+# Folder scan
+path ="/Users/jonah/PycharmProjects/digitalizeDocs"
+dirs = os.listdir(path)
+dirs.sort()
+checks = []
+# Liste an zu kontrollierenden Files erstellen
+for file in dirs:
+    if file.endswith('.pdf'):
+        checks.append(file)
+
+# define keyterms
+String1 = "Rechnung"
+String2 = "Angebot"
+String4 = "Test"
+for check in checks:
+    pdfFileObj = open(check, 'rb')
+    # open the pdf file
+    object = PyPDF4.PdfFileReader(pdfFileObj)
+    # get number of pages
+    NumPages = object.getNumPages()
+    # extract text and do the search
+    for i in range(0, NumPages):
+        PageObj = object.getPage(i)
+        Text = PageObj.extractText()
+        # print(Text)
+        ResSearch1 = search(String1, Text)
+        ResSearch2 = search(String1, Text)
+        if ResSearch1=="Rechnung":
+            #TODO Dann kontrolliere nach Datum
+            #anschließend speichere es ab
+        if ResSearch2== "Angebot":
+            # TODO s.o
+
+
+
